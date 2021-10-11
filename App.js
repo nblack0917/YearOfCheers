cfbb19fb020608bd480f0433152c83a1839586c6
nblack0917 <nickablack@gmail.com>
2021-10-11 15:00:34 -0500

    updated drawer nav

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Home Page</Text>
+      <Button
+        onPress={() => navigation.navigate("Cheers")}
+        title="Go to Cheers"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+const Cheers = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Cheers Page</Text>
+      <Button onPress={() => navigation.navigate("Home")} title="Go to Home" />
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to starfdssadfdsafdsafdsfdsang on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        // screenOptions={{
+        //   drawerActiveBackgroundColor: "#000",
+        //   drawerContentContainerStyle: { backgroundColor: "#333" },
+        //   drawerActiveTintColor: "#fff",
+        // }}
+      >
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Cheers" component={Cheers} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
